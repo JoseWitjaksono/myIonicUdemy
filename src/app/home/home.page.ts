fbc011d850210d733a69af846f01bb1fc45a550e
Jose Witjaksono <hpsx80@gmail.com>
2019-05-09 14:26:19 +0700

    Initial Commit - Run on Web & Android

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  constructor(public alertCtrl: AlertController, public loadingCtrl: LoadingController, public toastCtrl: ToastController){
+
+  }
+
+  async openPage(){
+    const alert = await this.alertCtrl.create({
+      header : 'Raccoon',
+      message : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc efficitur quam commodo semper mattis.',
+      buttons : ['Ok']
+    })
+    await alert.present();
+  }
+
+  async openLoading(){
+    const loading = await this.loadingCtrl.create({
+      message : 'Hello, i am your message !',
+      spinner : 'crescent',
+      duration : 2000
+    })
+    await loading.present();
+  }
+
+  async openToast(){
+    const toast = await this.toastCtrl.create({
+      message : 'Hello, i am your message !',
+      duration : 2000
+    })
+    await toast.present();
+  }
 }
